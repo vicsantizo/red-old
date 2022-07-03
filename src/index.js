9e29bc8c5af35be434063fccc192ad1225be3904
vicsantizo <santizo.vic@gmail.com>
2022-07-03 16:46:30 -0600

    Create event listener to remove books
    Create event listener which uses the concept of "event delegation" in order to avoid having to set the event listener on every child individually.

--- src/index.js
+++ src/index.js
@@ -41,3 +41,14 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
    
 });
 
+// Event: Remove a book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteBook(e.target);
+
+    // Remove book from the Store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+    // Show success message
+    UI.showAlert('Book Removed', 'success');
+});
