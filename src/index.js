2677d3a1dd3b55aa3d415102756ec336650e9d79
vicsantizo <santizo.vic@gmail.com>
2022-07-03 16:45:46 -0600

    Add event listener for adding books to the table

--- src/index.js
+++ src/index.js
@@ -8,3 +8,36 @@ import { Store } from './modules/books/store.js';
 import { UI } from './modules/books/ui.js';
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent Actual submit
+    e.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    
+    // Validate 
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+         // Instantiate book
+        const book = new Book(title, author, isbn);
+    
+        // Add Book to list
+        UI.addBookToList(book);
+
+        // Add book to store
+        Store.addBook(book);
+
+        // Show success message
+        UI.showAlert('Book Added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
+   
+});
+
