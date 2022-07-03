989a500221ef1a30c14bb1a3e96666a4829c05b7
vicsantizo <santizo.vic@gmail.com>
2022-07-03 16:26:56 -0600

    Import necessary modules (book & store)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/main.scss';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import { Book } from './modules/books.js';
+import { Store } from './modules/store.js';
