48c19bb29edf1229d96792d006c8fcd6a84d2ba5
vicsantizo <santizo.vic@gmail.com>
2022-07-03 16:44:30 -0600

    Add event listener to display stored books when the page loads

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import '@fortawesome/fontawesome-free/js/brands';
 import { Book } from './modules/books/book.js';
 import { Store } from './modules/books/store.js';
 import { UI } from './modules/books/ui.js';
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
