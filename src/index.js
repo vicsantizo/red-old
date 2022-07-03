e44f2e3d31feda848f633972896c5900381dd543
vicsantizo <santizo.vic@gmail.com>
2022-07-03 16:43:46 -0600

    Import `UI` class

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 import { Book } from './modules/books/book.js';
 import { Store } from './modules/books/store.js';
-
+import { UI } from './modules/books/ui.js';
