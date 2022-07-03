8b06eed717aa0cd4a8213d47fb7dde078c4f312f
vicsantizo <santizo.vic@gmail.com>
2022-07-03 16:35:25 -0600

    Fix import's paths

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
-import { Book } from './modules/books.js';
-import { Store } from './modules/store.js';
+import { Book } from './modules/books/book.js';
+import { Store } from './modules/books/store.js';
+
